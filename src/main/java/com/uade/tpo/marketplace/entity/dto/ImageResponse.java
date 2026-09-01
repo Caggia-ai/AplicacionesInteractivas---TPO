58532ebc1d5969d3a0a0a12d3bf843477731d327
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 18:21:04 -0300

    Remove @Builder annotation from ImageResponse
    Removed the Lombok @Builder annotation from ImageResponse class.

--- src/main/java/com/uade/tpo/marketplace/entity/dto/ImageResponse.java
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/ImageResponse.java
@@ -1,10 +1,8 @@
 package com.uade.tpo.marketplace.entity.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class ImageResponse {
   private Long id;
   private String file;
