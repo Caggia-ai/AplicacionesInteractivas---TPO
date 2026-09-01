2dac31aba593387ed1b460b83c65e0f0323b0ac7
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 18:18:18 -0300

    Add @Repository annotation to ImageRepository

--- src/main/java/com/uade/tpo/marketplace/repository/ImageRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.uade.tpo.marketplace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.uade.tpo.marketplace.entity.Image;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
