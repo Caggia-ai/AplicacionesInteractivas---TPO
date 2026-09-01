7b6350d7022f925f4a6eeb4453e47f99a6a0776b
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 17:45:16 -0300

    Se refactorizó addImageToProduct para usar MultipartFile

--- src/main/java/com/uade/tpo/marketplace/service/ImageServiceImpl.java
+++ src/main/java/com/uade/tpo/marketplace/service/ImageServiceImpl.java
@@ -2,11 +2,17 @@ package com.uade.tpo.marketplace.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.uade.tpo.marketplace.entity.Image;
 import com.uade.tpo.marketplace.entity.Product;
 import com.uade.tpo.marketplace.repository.ImageRepository;
 import com.uade.tpo.marketplace.repository.ProductRepository;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 
 @Service
 public class ImageServiceImpl implements ImageService {
@@ -18,13 +24,20 @@ public class ImageServiceImpl implements ImageService {
     private ProductRepository productRepository;
 
     @Override
-    public Image addImageToProduct(Long productId, String url) {
+    public Image addImageToProduct(Long productId, MultipartFile file) throws IOException, SQLException {
         Product product = productRepository.findById(productId).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
-            
+
+        Blob blob = new SerialBlob(file.getBytes());
+        
         Image image = new Image();
-        image.setUrl(url);
+        image.setImage(blob);
         image.setProduct(product); 
         
         return imageRepository.save(image);
+    }
+
+    @Override
+    public Image viewById(Long id) {
+        return imageRepository.findById(id).orElseThrow(() -> new RuntimeException("Imagen no encontrada"));
     }
 }
