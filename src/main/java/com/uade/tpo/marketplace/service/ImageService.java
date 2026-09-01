0f54990593d48a30ed82560a131fd06ab83c34d2
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 17:47:29 -0300

    Se modificó addImageToProduct para el uso de MultipartFile y se agregó viewById
    Updated addImageToProduct method to accept MultipartFile and handle exceptions.

--- src/main/java/com/uade/tpo/marketplace/service/ImageService.java
+++ src/main/java/com/uade/tpo/marketplace/service/ImageService.java
@@ -1,7 +1,12 @@
 package com.uade.tpo.marketplace.service;
 
 import com.uade.tpo.marketplace.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public interface ImageService {
-    Image addImageToProduct(Long productId, String url);
+    Image addImageToProduct(Long productId, MultipartFile file) throws IOException, SQLException;
+    Image viewById(Long id);
 }
