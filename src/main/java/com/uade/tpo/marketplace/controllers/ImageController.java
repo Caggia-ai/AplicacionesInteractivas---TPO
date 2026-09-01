b8722678b20152134bb013c4553780ccf70e1692
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 18:14:00 -0300

    Refactor ImageController to handle file uploads

--- src/main/java/com/uade/tpo/marketplace/controllers/ImageController.java
+++ src/main/java/com/uade/tpo/marketplace/controllers/ImageController.java
@@ -5,8 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.uade.tpo.marketplace.entity.Image;
-import com.uade.tpo.marketplace.entity.dto.ImageRequest;
+import com.uade.tpo.marketplace.entity.dto.AddFileRequest;
+import com.uade.tpo.marketplace.entity.dto.ImageResponse;
 import com.uade.tpo.marketplace.service.ImageService;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("images")
@@ -16,8 +21,20 @@ public class ImageController {
     private ImageService imageService;
 
     @PostMapping
-    public ResponseEntity<Image> addImage(@RequestBody ImageRequest request) {
-        Image image = imageService.addImageToProduct(request.getProductId(), request.getUrl());
-        return ResponseEntity.ok(image);
+    public ResponseEntity<String> addImage(AddFileRequest request) throws IOException, SQLException {
+        imageService.addImageToProduct(request.getProductId(), request.getFile());
+        return ResponseEntity.ok("created");
+    }
+
+    @GetMapping
+    public ResponseEntity<ImageResponse> displayImage(@RequestParam("id") Long id) throws IOException, SQLException {
+        Image image = imageService.viewById(id);
+        String encodedString = Base64.getEncoder()
+                .encodeToString(image.getImage().getBytes(1, (int) image.getImage().length()));
+
+        ImageResponse response = new ImageResponse();
+        response.setId(id);
+        response.setFile(encodedString);
+        return ResponseEntity.ok().body(response);
     }
 }
