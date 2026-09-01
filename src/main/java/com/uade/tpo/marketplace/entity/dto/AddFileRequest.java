c90d7cd08e7909c3d3c206667f236992765afbc2
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 17:27:26 -0300

    Se creó AddFileRequest.java en dto

--- src/main/java/com/uade/tpo/marketplace/entity/dto/AddFileRequest.java
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/AddFileRequest.java
@@ -1,1 +1,11 @@
+package com.uade.tpo.marketplace.entity.dto;
 
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class AddFileRequest {
+  private String name;
+  private MultipartFile file;
+  private Long productId;
+}
