e2b1e37063dea1b2ac025bbe0c82a2f939715a16
Caggia-ai <nicocaggia@gmail.com>
2026-09-01 18:18:33 -0300

    Remove @Repository annotation from ImageRepository
    Removed the @Repository annotation from ImageRepository.

--- src/main/java/com/uade/tpo/marketplace/repository/ImageRepository.java
+++ src/main/java/com/uade/tpo/marketplace/repository/ImageRepository.java
@@ -2,8 +2,6 @@ package com.uade.tpo.marketplace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.uade.tpo.marketplace.entity.Image;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
