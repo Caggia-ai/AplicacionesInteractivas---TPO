38c0e4143b207681a0bf39692ca764fbcbae6e5e
Caggia-ai <nicocaggia@gmail.com>
2026-09-03 08:48:26 -0300

    Se agregó UserPatchRequest DTO con los campos de Usuario

--- src/main/java/com/uade/tpo/marketplace/entity/dto/UserPatchRequest.java
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/UserPatchRequest.java
@@ -1,1 +1,12 @@
+package com.uade.tpo.marketplace.entity.dto;
+import lombok.Data;
 
+@Data
+public class UserPatchRequest{
+  private String username;
+  private String name; 
+  private String surname;
+  private String email;
+  private String password;
+  private String role;
+}
