1cfef43bc95308af6192a76d9b1a0f9d581e391a
Caggia-ai <nicocaggia@gmail.com>
2026-09-03 08:57:46 -0300

    Se refactorizó el método updateUser para considerar UserPatchRequest

--- src/main/java/com/uade/tpo/marketplace/service/UserService.java
+++ src/main/java/com/uade/tpo/marketplace/service/UserService.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import com.uade.tpo.marketplace.entity.User;
+import com.uade.tpo.marketplace.entity.dto.UserPatchRequest;
 import com.uade.tpo.marketplace.exceptions.UserDuplicateException;
 
 public interface UserService {
     public Page<User> getUsers(PageRequest pageRequest);
     public Optional<User> getUserById(Long userId);
     public User createUser(String username, String name, String surname, String email, String password, String role) throws UserDuplicateException;
-    public User updateUser(Long userId, String username, String name, String surname, String email, String password, String role) throws UserDuplicateException;
+    public User updateUser(Long userId, UserPatchRequest request) throws UserDuplicateException;
 }
