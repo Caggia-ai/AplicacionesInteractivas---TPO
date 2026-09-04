96e40bf6b2ac155934a06e1636b021970f940059
Caggia-ai <nicocaggia@gmail.com>
2026-09-04 18:49:45 -0300

    Add AuthenticationResponse DTO class

---
+++ src/main/java/com/uade/tpo/marketplace/entity/dto/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.uade.tpo.marketplace.entity.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String accessToken;
+}
