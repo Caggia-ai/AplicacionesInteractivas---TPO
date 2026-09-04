66bd4086c7ec2903fd15118da138d15133ef487e
Lucia Gabian <165518906+dinahverse@users.noreply.github.com>
2026-09-04 10:00:23 -0300

    Update ApplicationConfig.java

--- src/main/java/com/uade/tpo/marketplace/controllers/config/ApplicationConfig.java
+++ src/main/java/com/uade/tpo/marketplace/controllers/config/ApplicationConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.uade.tpo.demo.repository.UserRepository;
+import com.uade.tpo.marketplace.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
