f3a088297556492173acea4fc1c7b2477b8d43c1
Lucia Gabian <165518906+dinahverse@users.noreply.github.com>
2026-09-04 10:04:28 -0300

    Fix import path for Role entity in SecurityConfig

--- src/main/java/com/uade/tpo/marketplace/controllers/config/SecurityConfig.java
+++ src/main/java/com/uade/tpo/marketplace/controllers/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
-import com.uade.tpo.demo.entity.Role;
+import com.uade.tpo.marketplace.entity.Role;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
