8b57918beeee7300750eb5d1c8b430ddcffbda7d
Lucia Gabian <165518906+dinahverse@users.noreply.github.com>
2026-09-04 08:20:21 -0300

    Implement UserDetails in User entity

--- src/main/java/com/uade/tpo/marketplace/entity/User.java
+++ src/main/java/com/uade/tpo/marketplace/entity/User.java
@@ -1,52 +1,106 @@
 package com.uade.tpo.marketplace.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-// import jakarta.persistence.Table;
 import lombok.Data;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 @Entity
-public class User {
-    public User(String username, String name, String surname, String email, String password, String role) {
+public class User implements UserDetails {
+
+    public User(String username, String name, String surname, String email, String password, Role role) {
         this.username = username;
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.password = password;
         this.role = role;
-        this.state = true; 
+        this.state = true;
     }
 
-    public User() {} 
+    public User() {}
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_user;
+
     @Column(unique = true)
     private String username;
+
     @Column
     private String name;
+
     @Column
     private String surname;
+
     @Column
     private String email;
+
     @Column
     private String password;
+
+    @Enumerated(EnumType.STRING)
     @Column
-    private String role;
+    private Role role;
+
     @Column
     private boolean state;
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Product> products;
+
+    // ---- Métodos que pide UserDetails (Spring Security) ----
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return state;
+    }
+}
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
